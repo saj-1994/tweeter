cba7e896370003226572487e6a3a241af065df57
Sayinthan Theivasothy <t.saj1994@gmail.com>
2020-03-25 16:17:38 +0000

    Code review

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,6 +2,7 @@ $(document).ready(() => {
   $('#tweet-text').on('keyup', function(evt) {
     let output = 140 - this.value.length;
     let counter = $(this).parents('.new-tweet-form').children('.counter');
+    
     if(output < 0) {
       counter.text(output);
       counter.addClass("red");
@@ -9,5 +10,5 @@ $(document).ready(() => {
       counter.removeClass("red");
       counter.text(output);
     }
-  })
-})
+  });
+});
