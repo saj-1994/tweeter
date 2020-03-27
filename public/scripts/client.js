dc1e1a08e449c9316769278acc1823fbdf0459c1
Sayinthan Theivasothy <t.saj1994@gmail.com>
2020-03-27 03:08:00 +0000

    reviewed used eslinter and prettier

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,8 +3,8 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const escape =  function(str) {
-  let div = document.createElement('div');
+const escape = function(str) {
+  let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
@@ -13,15 +13,15 @@ const escape =  function(str) {
 const renderTweets = tweets => {
   for (const tweet of tweets) {
     const $tweet = createTweetElement(tweet);
-    $('#tweets-container').prepend($tweet);
+    $("#tweets-container").prepend($tweet);
   }
 };
 
 // creates the html markup using the tweet data
 const createTweetElement = tweet => {
-  const {name, avatars, handle} = tweet.user;
-  const {text} = tweet.content;
-  const {created_at} = tweet;
+  const { name, avatars, handle } = tweet.user;
+  const { text } = tweet.content;
+  const { created_at } = tweet;
   const daysSincePost = getNumberOfDays(created_at);
   const $tweetMarkup = `
   <article class="tweet">
@@ -60,38 +60,36 @@ const postTweets = (form, callback) => {
   const $form = form;
   const data = $form.serialize();
   $.ajax({
-    url: $form.attr('action'),
+    url: $form.attr("action"),
     type: "POST",
     data: data
-  })
-    .then(() => {
-      callback();
-    });
+  }).then(() => {
+    callback();
+  });
 };
 
 const getTweetErrMsg = (tweet, counter) => {
-  if (tweet === '' || tweet === null) {
-    return 'The tweet field is empty, please enter a valid tweet.';
+  if (tweet === "" || tweet === null) {
+    return "The tweet field is empty, please enter a valid tweet.";
   } else if (counter < 0) {
-    return 'The tweet is too long, please enter tweet below 140 characters.';
+    return "The tweet is too long, please enter tweet below 140 characters.";
   }
 };
 
 // sends a GET request to get all the tweets stored in the DB
-const loadTweets = (callback) => {
+const loadTweets = callback => {
   $.ajax({
-    url: '/tweets/',
-    type: 'GET',
-    dataType: 'JSON'
-  })
-    .then(res => {
-      callback(res);
-    });
+    url: "/tweets/",
+    type: "GET",
+    dataType: "JSON"
+  }).then(res => {
+    callback(res);
+  });
 };
 
 // gets the last tweet posted in the DB
 const loadLastTweet = () => {
-  loadTweets((tweets) => {
+  loadTweets(tweets => {
     const lastTweetInd = tweets.length - 1;
     const lastTweet = [];
     lastTweet.push(tweets[lastTweetInd]);
@@ -101,43 +99,42 @@ const loadLastTweet = () => {
 
 // executes only when the DOM is fully loaded
 $(document).ready(() => {
-  loadTweets((tweets) => {
+  loadTweets(tweets => {
     renderTweets(tweets);
   });
 
   // executes when the form tries to submit
-  $('#new-tweet-form').on('submit', function(evt) {
+  $("#new-tweet-form").on("submit", function(evt) {
     evt.preventDefault();
     const form = $(this);
     // traverses to the children nodes of the current node
-    const counter = form.children('#counter').val();
-    const tweet = form.children('#tweet-text').val();
+    const counter = form.children("#counter").val();
+    const tweet = form.children("#tweet-text").val();
     const tweetErr = getTweetErrMsg(tweet, counter);
-    
-    $('#error').slideUp();
+
+    $("#error").slideUp();
     // checks user input for errors and posts if no errors else shows error msg
     if (!tweetErr) {
       postTweets(form, () => {
         loadLastTweet();
       });
       $(form)[0].reset();
-      $(form.children('#counter')).text(140);
+      $(form.children("#counter")).text(140);
     } else {
-      $('#error-msg').text(tweetErr);
-      $('#error').slideDown();
+      $("#error-msg").text(tweetErr);
+      $("#error").slideDown();
     }
   });
 
   // toggles the new tweet form when new tweet button clicked
-  $('#new-tweet-toggle, #navbar-arrow').on('click', function() {
-    const $newTweet = $('#new-tweet');
-    if ($newTweet.is(':visible')) {
+  $("#new-tweet-toggle, #navbar-arrow").on("click", function() {
+    const $newTweet = $("#new-tweet");
+    if ($newTweet.is(":visible")) {
       $newTweet.slideUp();
     } else {
       $newTweet.slideDown(() => {
-        $('#tweet-text').focus();
+        $("#tweet-text").focus();
       });
     }
   });
 });
-
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,11 @@
 $(document).ready(() => {
-  $('#tweet-text').on('keyup', function(evt) {
+  $("#tweet-text").on("keyup", function() {
     const output = 140 - $(this).val().length;
-    const $counter = $(this).parents('#new-tweet-form').children('#counter');
-    
-    if(output < 0) {
+    const $counter = $(this)
+      .parents("#new-tweet-form")
+      .children("#counter");
+
+    if (output < 0) {
       $counter.text(output);
       $counter.addClass("new-tweet-negative-counter");
     } else {
